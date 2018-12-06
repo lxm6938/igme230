b55ceffa1197cd7de7faad28ca287239d09bfd72
Liam Madigan <LiamMadAgain@liams-mbp.wireless.rit.edu>
2018-12-06 15:14:40 -0500

    Liam Madigan - Final Exam

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -1,22 +1,23 @@
 $(document).ready(() => {
-    $('.list').css("display", "none")
+    $('.list').css("display", "none") //hiding submenus on load
 
-    let article0 = ("article0.txt")
+    let article0 = ("article0.txt") //loading article0 on load
     $('article').load(article0)
 })
 
-$('.top').on('click', function(){
-    $(this).next().slideToggle()
+$('.top').on('click', function(){ //when a menu is clicked
+    $(this).next().slideToggle() //slide the submenu out
 })
 
-$("input[type='radio'][name='story']").on('click', (el) => {
-    if(el.target.value == "article1.txt"){
-        let article1 = ("article1.txt")
+
+$("input[type='radio'][name='story']").on('click', (el) => { //when a radio button is clicked
+    if(el.target.value == "article1.txt"){ //if the value is article 1
+        let article1 = ("article1.txt") //load article1.txt into the article
         $('article').load(article1)
     }
 
     else{
-        let article2 = ("article2.txt")
-        $('article').load(article2)
+        let article2 = ("article2.txt") //for anything else (the other radio button)
+        $('article').load(article2) //load article2.txt into article
     }
 })
